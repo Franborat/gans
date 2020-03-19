7a5001ac3c0ac989075f45a7cbc5c33ffba5cab1
fran <fran@MacBook-Air-Aleksandra.local>
2020-03-19 15:30:34 +0100

    ConvBlock and TransposeConvBlock classes

--- network.py
+++ network.py
@@ -36,27 +36,41 @@ class GANLoss(nn.Module):
     """
 
 
-class TransposeConvBlock(nn.Module):
-    """
-    Define a Transpose Convolutional building block
-    """
-    def __init__(self):
-        super(TransposeConvBlock, self).__init__()
-
-    def forward(self, x):
-        return
-
-
-
 class ConvBlock(nn.Module):
     """
-    Define a Convolutional building block
+    Define a Convolutional building block (Excluding input and output layer)
     """
-    def __init__(self):
+
+    def __init__(self, in_channels, out_channels):
         super(ConvBlock, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
 
-    def forward(self, x):
-        return
+    def forward(self, input):
+        return self.main(input)
 
 
+class TransposeConvBlock(nn.Module):
+    """
+    Define a Transpose Convolutional building block (Excluding input and output layer)
+    """
 
+    def __init__(self, in_channels, out_channels):
+        """
+
+        Args:
+            in_channels: Number of channels entering the transpose conv
+            out_channels: Number of channels after the transpose conv
+        """
+        super(TransposeConvBlock, self).__init__()
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, input):
+        return self.main(input)
