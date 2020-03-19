3a490b79d6220a3250f2446ce51268c63fdbde78
fran <fran@MacBook-Air-Aleksandra.local>
2020-03-19 12:52:22 +0100

    Start defining the skeleton functions and classes in network.py

---
+++ network.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+
+
+# Functions
+
+def define_G():
+    """
+    Create a Discriminator
+    Returns: Generator Network
+
+    """
+
+
+def define_D():
+    """
+    Create a Discriminator
+    Returns: Discriminator network
+
+    """
+
+
+def init_weights():
+    """
+    Initialize network weights
+    Returns:
+
+    """
+
+
+# Classes
+
+class GANLoss(nn.Module):
+    """
+    Define different GAN objective
+    """
+
+
+class TransposeConvBlock(nn.Module):
+    """
+    Define a Transpose Convolutional building block
+    """
+    def __init__(self):
+        super(TransposeConvBlock, self).__init__()
+
+    def forward(self, x):
+        return
+
+
+
+class ConvBlock(nn.Module):
+    """
+    Define a Convolutional building block
+    """
+    def __init__(self):
+        super(ConvBlock, self).__init__()
+
+    def forward(self, x):
+        return
+
+
+
