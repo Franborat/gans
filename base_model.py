aa90fb1c70aef6e8c4f595dbf77ce38c2a1914d0
fran <fran@MacBook-Air-Aleksandra.local>
2020-03-26 11:58:35 +0100

    Base model abstract class with the 5 methods to implement, get_scheduler in network.py

---
+++ base_model.py
@@ -0,0 +1,95 @@
+import os
+from abc import ABC, abstractmethod
+import torch
+import network
+
+
+class BaseModel(ABC):
+    """
+    Abstract base class (ABC) for models.
+    To create a subclass, you need to implement the following five methods:
+    -- <__init__>: Class initializer
+    -- <set_input>: Unpack data from dataset and apply preprocessing
+    -- <forward>: Produce intermediate results
+    -- <optimize_parameters>: Calculate losses, gradients, and upgrade network weights
+    -- <modify_commandline_options>: (optionally) add model-specific options and set default options
+    """
+
+    def __add__(self, opt):
+        """ Initialize the BaseModel class
+
+        Args:
+            opt: (Option class) -- Stores all the experiment flags
+
+        When creating a custom class:
+            1st: call <BaseModel.__init__(self, opt)>
+            2nd: define four lists:
+                self.loss_names (str list): training losses to display and save
+                self.model_names (str lists): networks used in our training
+                self.visual_names (str lists): images to display and save
+                self.optimizers (optimizer list): define and initialize optimizers
+
+        Returns:
+
+        """
+
+        self.opt = opt
+        self.gpu_ids = opt.gpu_ids
+        self.isTrain = opt.isTrain
+        self.device = torch.device = torch.device('cuda:{}'.format(self.gpu_ids[0])) if self.gpu_ids \
+            else torch.device('cpu')
+        self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)
+        if opt.preprocess != 'scale_width':
+            torch.backends.cudnn.benchmark = True
+
+        # To be defined when creating a custom class
+        self.loss_names = []
+        self.model_names = []
+        self.visual_names = []
+        self.optimizers = []
+        self.image_paths = []
+        self.metric = 0 # used for learning rate policy 'plateau'
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        """
+        Add model-specific options, and rewrite default values for existing options
+        Args:
+            parser: original option parser
+            is_train: whether training/test phase. Use flag to add training or test-specific options
+
+        Returns:
+            The modified parser
+        """
+        return parser
+
+    @abstractmethod
+    def set_input(self, input):
+        """
+        Unpack input data from the dataloader and perform necessary pre-processing steps
+        Args:
+            input: (dict): includes the data itself and its metadata information
+
+        Returns:
+
+        """
+        pass
+
+    @abstractmethod
+    def forward(self):
+        """ Run forward pass; called by both functions <optimize_parameters> and <test> """
+        pass
+
+    @abstractmethod
+    def optimize_parameters(self):
+        """ Calculate losses, gradients, and update network weights; called every training iteration """
+    pass
+
+    def setup(self, opt):
+        """
+        Load and print networks; print schedulers
+        Args:
+            opt: (option class)
+        """
+        if self.isTrain:
+            self.schedulers = [network.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
--- network.py
+++ network.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 
 # Functions
+from torch.optim import lr_scheduler
+
 
 def define_G(input_nc, ngf, output_nc):
     """
@@ -47,11 +49,11 @@ def define_D(input_nc, ngf, output_nc):
         # Input layer
         nn.Conv2d(in_channels=input_nc, out_channels=ngf, kernel_size=4, stride=2, padding=1, bias=False),
         # Conv blocks
-        ConvBlock(in_channels=ngf, out_channels=ngf*2),
-        ConvBlock(in_channels=ngf*2, out_channels=ngf*4),
-        ConvBlock(in_channels=ngf*4, out_channels=ngf*8),
+        ConvBlock(in_channels=ngf, out_channels=ngf * 2),
+        ConvBlock(in_channels=ngf * 2, out_channels=ngf * 4),
+        ConvBlock(in_channels=ngf * 4, out_channels=ngf * 8),
         # Output layer
-        nn.Conv2d(in_channels=ngf*8, out_channels=output_nc, kernel_size=4, stride=1, padding=0, bias=False)
+        nn.Conv2d(in_channels=ngf * 8, out_channels=output_nc, kernel_size=4, stride=1, padding=0, bias=False)
     )
     init_weights(discriminator_net)
     return discriminator_net
@@ -72,12 +74,41 @@ def init_weights(net):
         nn.init.constant_(net.bias.data, 0)
 
 
+def get_scheduler(optimizer, opt):
+    """
+
+    Args:
+        optimizer: Optimizer of the network
+        opt: opt.lr_policy stores the name of the learning rate policy
+
+    Returns:
+
+    """
+    # Keep the same lr for the first opt.n_epochs and then decay for the next opt.n_epochs_decay
+    if opt.lr_policy == 'linear':
+        def lambda_rule(epoch):
+            lr_l = 1.0 - max(0, epoch + opt.epoch_count - opt.n_epochs) / float(opt.n_epochs_decay + 1)
+            return lr_l
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
+    elif opt.lr_policy == 'step':
+        scheduler = lr_scheduler.StepLR(optimizer, step_size=opt.lr_decay_iters, gamma=0.1)
+    # Reduce learning rate when a metric has stopped improving
+    elif opt.lr_policy == 'plateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
+    elif opt.lr_policy == 'cosine':
+        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=opt.n_epochs, eta_min=0)
+    else:
+        return NotImplementedError('learning rate policy [%s] is not implemented', opt.lr_policy)
+    return scheduler
+
+
 # Classes
 
 class GANLoss(nn.Module):
     """
     Define different GAN objective
     """
+
     def __init__(self, gan_mode):
         """
 
