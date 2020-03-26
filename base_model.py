79fc0e0a10ef44fa19e89cc027cc9500463d6721
fran <fran@MacBook-Air-Aleksandra.local>
2020-03-26 15:57:37 +0100

    Define training loop: forward, backward, optimize_parameters

--- base_model.py
+++ base_model.py
@@ -93,3 +93,16 @@ class BaseModel(ABC):
         """
         if self.isTrain:
             self.schedulers = [network.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
+
+    def set_requires_grad(self, nets, requires_grad=False):
+        """Set requies_grad=Fasle for all the networks to avoid unnecessary computations
+        Parameters:
+            nets (network list)   -- a list of networks
+            requires_grad (bool)  -- whether the networks require gradients or not
+        """
+        if not isinstance(nets, list):
+            nets = [nets]
+        for net in nets:
+            if net is not None:
+                for param in net.parameters():
+                    param.requires_grad = requires_grad
--- dcgan_model.py
+++ dcgan_model.py
@@ -19,8 +19,8 @@ class DCGANModel(BaseModel):
 
         BaseModel.__init__(self, opt)
 
-        self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake']
-        self.visual_names = ['real_A', 'fake_B', 'real_B']
+        self.loss_names = ['G_GAN', 'D_real', 'D_fake']
+        self.visual_names = ['noise', 'fake', 'real']
         if self.isTrain:
             self.model_names = ['G', 'D']
         else:
@@ -43,10 +43,63 @@ class DCGANModel(BaseModel):
             self.optimizers.append(self.optimizer_D)
 
     def set_input(self, input):
-        pass
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+        Parameters:
+            input (dict): include the data itself and its metadata information.
+        The option 'direction' can be used to swap images in domain A and domain B.
+        """
+        self.real = input['real'].to(self.device)
+        self.noise = input['noise'].to(self.device)
+        self.image_paths = input['paths']
 
     def forward(self):
+        """Run forward pass (Only for the G) ; called by both functions <optimize_parameters> and <test>"""
+        self.fake = self.netG(self.noise)  # G(noise)
         pass
 
+    def backward_D(self):
+        """Calculate GAN Loss for the D by:
+            1st: Run a forward pass through D
+            2nd: Calculate loss
+            3rd: Backpropagate loss
+        """
+        # Train with all-fake batch
+        pred_fake = self.netD(self.fake.detach())
+        self.loss_D_fake = self.criterionGAN(pred_fake, False)
+
+        # Train with all-real batch
+        pred_real = self.netD(self.real)
+        self.loss_D_real = self.criterionGAN(pred_real, True)
+
+        # Combine loss and calculate gradients
+        self.loss_D = (self.loss_D_real + self.loss_D_fake) * 0.5
+        self.loss_D.backward()
+
+    def backward_G(self):
+        """ Calculate GAN Loss for G by:
+            1st: Calculate predictions of D when passing fake images
+            2nd: Calculate loss (for G is that fake images are predicted as True)
+            3rd: Backpropagate loss
+
+        """
+        # Train with all-fake batch
+        pred_fake = self.netD(self.fake.detach())
+        self.loss_G_GAN = self.criterionGAN(pred_fake, True)
+        self.loss_G_GAN.backward()
+
+
     def optimize_parameters(self):
+        self.forward()
+        # Update D
+        self.set_requires_grad(self.netD, True)  # enable backprop for D
+        self.optimizer_D.zero_grad()  # set D's gradients to 0
+        self.backward_D()  # calculate gradients for D
+        self.optimizer_D.step()  # update D's weights
+
+        # Update G
+        self.set_requires_grad(self.netD, False)  # D needs no gradients when training G
+        self.optimizer_G.zero_grad()  # set G's gradients to 0
+        self.backward_G()  # calculate gradients for G
+        self.optimizer_G.step()  # update D's weights
+
         pass
